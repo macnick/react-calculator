eafc1dbbb8aa5bc79c5d435f974ecf80f1139ed1
macnick <nick@macexperts.gr>
2020-07-01 21:26:17 +0300

    Create components folder. Implement App.js and Display.js

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+
+class App extends React.Component {
+  render() {
+    <div>
+      <Display />
+      <ButtonPanel />
+    </div>;
+  }
+}
---
+++ src/components/Display.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Display(props) {
+  return <h1>{props.result}</h1>;
+}
+
+Display.propTypes = {
+  result: PropTypes.string,
+};
+
+Display.defaultProps = {
+  result: 0,
+};
+export default Display;
