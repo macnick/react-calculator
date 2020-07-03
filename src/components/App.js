20989bd85ef22b106096920f5dfbc6accf560b4e
macnick <nick@macexperts.gr>
2020-07-03 14:22:54 +0300

    Add basic styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import '../styles/main.css';
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Display />
       <ButtonPanel />
     </div>
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Button({ name }) {
-  return <div>{name}</div>;
+  return <div className="button">{name}</div>;
 }
 
 Button.propTypes = {
