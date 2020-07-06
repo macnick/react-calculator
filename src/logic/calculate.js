b8040077c0c1a8a0f28f1e2693cba601396c444f
macnick <nick@macexperts.gr>
2020-07-06 20:32:02 +0300

    Fix operate module

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,5 +1,14 @@
-const calculate = () => {
+import operate from './operate';
 
+const calculate = (data, btnName) => {
+  let { total, next, operation } = data;
+  const operations = {
+    '+/-': () => { total *= -1; next *= -1 },
+    '-': () => next *= -1,
+    '*': () => total * next
+  }
+  operations[btnName]();
+  return { total, next, operation }
 }
 
 export default calculate;
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,5 +1,20 @@
-const calculate = () => {
-  console.log(5);
-}
+import Big from 'big.mjs';
 
-export default calculate;
+const operate = (number1, number2, operation) => {
+  let num1 = Big(number1);
+  let num2 = Big(number2);
+  let total = 0;
+  let operations = {
+    '+': () => num1.plus(num2),
+    '-': () => num1.minus(num2),
+    x: () => num1.times(num2),
+    '÷': () => (num2 === 0 ? 'NaN' : num1.div(num2)),
+    '%': () => (!num2 ? num1.time(0.01) : num1.times(num2).times(0.01)),
+  };
+  // do I need to convert the results to strings inside the object?
+  total = operations[operation]();
+
+  return total.toString();
+};
+
+export default operate;
