63b991eef4ce44f0e7fb0628ea68060849315588
macnick <nick@macexperts.gr>
2020-07-10 15:07:37 +0300

    Fix zeroes bug

--- src/components/App.js
+++ src/components/App.js
@@ -28,6 +28,7 @@ class App extends React.Component {
 
     return (
       <div className="app">
+        <pre>{JSON.stringify(this.state, null, 3)}</pre>
         <Display result={result} />
         <ButtonPanel clickHandler={this.handleClick} />
       </div>
